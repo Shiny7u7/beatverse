645d041720edd61a21b99a571fc77c0eaf36e85d
Shiny7u7 <165960359+Shiny7u7@users.noreply.github.com>
2024-11-23 13:15:44 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    SECRET_KEY = "tu_secreto"  # Cambia esto por un valor seguro
+    MYSQL_HOST = "34.134.65.19"
+    MYSQL_USER = "taco"
+    MYSQL_PASSWORD = "12345"
+    MYSQL_DB = "musica"
+    MYSQL_PORT = 3306
